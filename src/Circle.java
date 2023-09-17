9e930e902f25bac088ec413248c44d311f5b2876
siegengin <109764860+siegengin@users.noreply.github.com>
2023-09-17 10:48:21 -0400

    Update Circle.java

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,73 @@
-package PACKAGE_NAME;public class Circle {
+import java.util.Objects;
+
+public class Circle extends GeometricObject implements Comparable<Circle> {
+    private double radius;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        super(color, filled);
+        if (radius <= 0) {
+            throw new IllegalArgumentException("Radius must be positive");
+        }
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        if (radius <= 0) {
+            throw new IllegalArgumentException("Radius must be positive");
+        }
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    public double getDiameter() {
+        return 2 * radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * radius * Math.PI;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Circle)) return false;
+        Circle circle = (Circle) o;
+        return Double.compare(circle.radius, radius) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(radius);
+    }
+
+    @Override
+    public int compareTo(Circle o) {
+        return Double.compare(this.radius, o.radius);
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", color='" + getColor() + '\'' +
+                ", filled=" + isFilled() +
+                ", date created=" + getDateCreated() +
+                '}';
+    }
 }
